834468d1c9540c9c71176ca5040095422a22a5ee
Ayush Srivastava <ayushsr0@gmail.com>
2024-04-09 19:01:45 +0530

    First Commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.jpg")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend</h3>
+              <br />
+              <p>
+              With a passion for crafting dynamic and user-centric experiences, bringing expertise to
+              the world of web development. Specializing in frontend technologies, I have a proven track record of building responsive and 
+              optimized sites.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend</h3>
+              <br />
+              <p>
+              With a foundation in backend technologies and a willingness to learn more, I'm eager to complement my frontend skills. I am open to expanding my knowledge further and contributing effectively to backend aspects of web development.     
+              </p>
+            </div>
+          </li>
+          {/* <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I have designed multiple landing pages and have created design
+                systems as well
+              </p>
+            </div>
+          </li> */}
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Ayush</h1>
+        <p className={styles.description}>
+        Experienced front-end developer with 2.6 years in React and JavaScript. 
+        Let's connect and explore my skills in crafting dynamic user interfaces !          
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/photo.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
