33af3112654e31ec0f1bad5905a8973cd5cadd2b
ayushsri2 <ayushsr0@gmail.com>
2024-07-15 22:59:37 +0530

    Made changes to React app

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Ayush</h1>
         <p className={styles.description}>
-        Experienced front-end developer with 2.6 years in React and JavaScript. 
+        Experienced front-end developer with 3 years in React and JavaScript. 
         Let's connect and explore my skills in crafting dynamic user interfaces !          
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
